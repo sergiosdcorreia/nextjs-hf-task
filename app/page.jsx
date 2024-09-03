4fa1a31a25942ad70aa0bc0e02f45bb548215f77
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-09-03 16:11:26 +0100

    Cleaning boilerplate. Adding logo.

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Image
+				src="/txo_logo_2.svg"
+				alt="TXO Logo"
+				width={500}
+				height={500}
+			/>
+    </main>
+  );
+}
