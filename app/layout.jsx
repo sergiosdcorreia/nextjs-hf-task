c0f26484d9310a406af428f1b092cbe1cb66a6c0
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-09-12 11:19:38 +0100

    Updating metadata.

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
