fc9b10e442c057d9caddfbfad46353049b877226
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-09-15 16:09:02 +0100

    Naming the hero section. Converting PXs to EMs on carousel loading.

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,11 @@ import Carousel from "./components/carousel";
 export default function Home() {
   return (
     <main>
-			<section className="section-container">
-				<h1 className="heading-1">We&apos;re hands-on and commited to evolving your business to strive for better.</h1>
-        <div className="offerings">
-          <h2 className="offerings_title">Our offering</h2>
-          <p className="offerings_body">We provide physical, pragmatic and personal business guidance. Success is never static. Neither are we. We&apos;ll be your business partners for positive change.</p>
+			<section className="hero">
+				<h1 className="hero_title">We&apos;re hands-on and commited to evolving your business to strive for better.</h1>
+        <div className="hero_offerings">
+          <h2 className="hero_offerings_title">Our offering</h2>
+          <p className="hero_offerings_body">We provide physical, pragmatic and personal business guidance. Success is never static. Neither are we. We&apos;ll be your business partners for positive change.</p>
         </div>
 			</section>
       <Carousel />
