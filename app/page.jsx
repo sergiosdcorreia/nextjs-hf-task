d09b3beb47fb1cf1e0f98cdc3e5fc245e3754676
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-09-18 21:54:31 +0100

    Animating Hero section's grid on window resize media queries.

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,60 @@
+'use client';
+
+import { useRef, useEffect } from 'react';
+import { wrapGrid } from 'animate-css-grid';
 import Carousel from "./components/carousel";
 
 export default function Home() {
+  const heroRef = useRef(null);
+  const mediaQuery1 = window.matchMedia('(min-width: 768px)');
+  const mediaQuery2 = window.matchMedia('(min-width: 1280px)');
+
+useEffect(() => {
+  if (heroRef.current) {
+    const { forceGridAnimation } = wrapGrid(heroRef.current, {
+      duration: 500,
+      easing: 'easeInOut',
+    });
+
+    const handleMediaQueryChange = () => {
+      console.log('Media query changed!')
+      if (heroRef.current) {
+        heroRef.current.style.gridTemplateColumns = mediaQuery1.matches
+          ? mediaQuery2.matches
+            ? 'repeat(8, 1fr)'
+            : 'repeat(5, 1fr)'
+          : '1fr';
+        
+        forceGridAnimation()
+        heroRef.current.offsetHeight;
+      }
+    };
+
+    // Set initial grid layout based on media query
+    handleMediaQueryChange();
+
+    mediaQuery1.addEventListener('change', handleMediaQueryChange);
+    mediaQuery2.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      // Cleanup media query listeners
+      mediaQuery1.removeEventListener('change', handleMediaQueryChange);
+      mediaQuery2.removeEventListener('change', handleMediaQueryChange);
+    };
+  }
+}, [mediaQuery1, mediaQuery2]);
+
   return (
     <main>
-			<section className="hero">
-				<h1 className="hero_title">We&apos;re hands-on and commited to evolving your business to strive for better.</h1>
-        <div className="hero_offerings">
-          <h2 className="hero_offerings_title">Our offering</h2>
-          <p className="hero_offerings_body">We provide physical, pragmatic and personal business guidance. Success is never static. Neither are we. We&apos;ll be your business partners for positive change.</p>
+			<section ref={heroRef} className="hero">
+        <div className="hero_title-container">
+				  <h1 className="hero_title">We&apos;re hands-on and commited to evolving your business to strive for better.</h1>
+        </div>
+        <div className="hero_offerings-container">
+          <div className="hero_offerings">
+            <h2 className="hero_offerings_title">Our offering</h2>
+            <p className="hero_offerings_body">We provide physical, pragmatic and personal business guidance. Success is never static. Neither are we. We&apos;ll be your business partners for positive change.</p>
+          </div>
         </div>
 			</section>
       <Carousel />
